c304c547cf6d8f12711726d2c315f8ea7047ca22
y-u-eun <yueun0706@gmail.com>
2025-04-07 14:17:56 +0900

    Add files via upload

--- lotto(bubble).c
+++ lotto(bubble).c
@@ -28,14 +28,14 @@ void main()
     }
     /*bubble sort*/
     int tmp = 0;
-    for (int i = 0; i < 6; i++) {
-        for (int j = 0; j < 6; j++) {
-          if (numarr[i] < numarr[j]) {
-            tmp = numarr[i];
-            numarr[i] = numarr[j];
-            numarr[j] = tmp;
-            }
-        tmp = 0;  
+    for (int i = 5; i >= 0; i--) {
+        for (int j = 0; j < i; j++) {
+          if (numarr[j] > numarr[j + 1]) {
+              tmp = numarr[j];
+              numarr[j] = numarr[j + 1];
+              numarr[j + 1] = tmp;
+          }
+          tmp = 0;
         }
     }
     for (int k = 0; k < 6; k++) {
