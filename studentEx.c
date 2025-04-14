8ced75aa9ef28189ccdf0300dd618601654f19dc
y-u-eun <yueun0706@gmail.com>
2025-04-14 12:28:14 +0900

    Add files via upload

--- studentEx.c
+++ studentEx.c
@@ -3,13 +3,13 @@
 #include <stdlib.h>
 #define MAX 1000
 #define TRUE 1
-// strcpy()와 strlen() 함수를 사용하기 위해 <string.h> 사용
+// strcpy()와 strlen(), strcmp() 함수를 사용하기 위해 <string.h> 사용
 // malloc()과 free() 함수를 사용하기 위해 <stdlib.h> 사용
 
 struct student {
         char *name;
         int age;
-        char sex;
+        char *sex;
         char *hob;
     };
 
@@ -35,19 +35,25 @@ void stprint(struct student *p) {
         
         printf("\n나이를 입력하세요. ");
         scanf("%d", &p->age);
+        // 나이는 포인터변수로 선언하지 않았으므로 malloc()을 이용해 공간을
+        // 할당하지 않고 주소값을 이용해 바로 저장할 수 있음.
         
         while (TRUE) {
             printf("\n성별을 입력하세요(M/F). ");
-            getchar();
-            scanf("%c", &p->sex);
-            if (p->sex == 'M' || p->sex == 'F') {
+            scanf("%s", tmp);
+            // strcmp()는 문자열을 비교하는 함수, 같으면 0을 반환
+            if (strcmp(tmp, "M") == 0 || strcmp(tmp, "m") == 0 || strcmp(tmp, "male") == 0) {
+                strcpy(tmp, "M");
+                break;
+            } else if (strcmp(tmp, "F") == 0 || strcmp(tmp, "f") == 0 || strcmp(tmp, "female") == 0) {
+                strcpy(tmp, "F");
                 break;
             } else {
-                printf("\n성별은 'M' 또는 'F'로 입력해주세요.\n");
+                printf("\n성별은 'M/F' 또는 'm/f' 또는 'male/female'로 입력해주세요.\n");
             }
         }
-        // 나이와 성별은 포인터변수로 선언하지 않았으므로 malloc()을 이용해 공간을
-        // 할당하지 않고 주소값을 이용해 바로 저장할 수 있음.
+        p->sex = malloc(strlen(tmp) + 1);
+        strcpy(p->sex, tmp);
         
         printf("\n취미를 입력하세요. ");
         scanf("%s", tmp);
@@ -58,7 +64,7 @@ void stprint(struct student *p) {
         
         printf("\n===학생정보시스템===\n");
         for (struct student *p2 = pp; p2 < p; p2++) {
-            printf("%d. %-s %-d %-c %-s\n", p2 - pp + 1, p2->name, p2->age, p2->sex, p2->hob);
+            printf("%d. %-s %-d %-s %-s\n", p2 - pp + 1, p2->name, p2->age, p2->sex, p2->hob);
             // p는 현재까지 입력된 값의 주소
             // p2는 구조체배열 s의 시작 주소부터 p까지 1씩 증가
             // 출력할 값의 주소(=p2)에서 구조체 배열 s의 주소(=s)를 빼줘야
@@ -69,7 +75,7 @@ void stprint(struct student *p) {
     
     // 구조체 포인터에 malloc()으로 메모리를 할당해서 사용한 후
     // 메모리 누수를 방지하기 위해 free()를 이용해 할당한 메모리를 해제해 줘야 함.
-    for (struct student *p3 = pp; p3 <= p; p3++) {
+    for (struct student *p3 = pp; p3 < p; p3++) {
         free(p3->name);
         free(p3->hob);
     }
@@ -79,13 +85,13 @@ void main() {
     
     printf("===학생정보시스템===\n");
     struct student s [MAX] = {
-        {"hong", 30, 'M', "soccer"},
-        {"kim", 25, 'M', "music"},
-        {"park", 20, 'F', "movie"}
+        {"hong", 30, "M", "soccer"},
+        {"kim", 25, "M", "music"},
+        {"park", 20, "F", "movie"}
     };
     
     for (int k = 0; k <= 2; k++) {
-            printf("%d. %-s %-d %-c %-s\n", k + 1, s[k].name, s[k].age, s[k].sex, s[k].hob);
+            printf("%d. %-s %-d %-s %-s\n", k + 1, s[k].name, s[k].age, s[k].sex, s[k].hob);
         }
     
     stprint(&s);    // stprint()에 구조체 배열 s의 주소를 전달
